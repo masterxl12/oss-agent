4c9e1bd9c26ec4cb3b89d4069e60c09d315d785e
xl <leixiao565566@163.com>
2020-03-12 14:23:13 +0800

    批量上传删除单

--- src/main/java/com/huayun/ossagent/controller/FileController.java
+++ src/main/java/com/huayun/ossagent/controller/FileController.java
@@ -32,13 +32,36 @@ public class FileController {
     }
 
     @RequestMapping(value = "/deleteBucket", method = RequestMethod.POST)
-    public java.lang.String deleteBucket(@RequestParam String bucketName) {
+    public String deleteBucket(@RequestParam String bucketName) {
         return fileService.deleteBucket(bucketName);
     }
 
     @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    public String upload(@RequestParam String bucketName, @RequestParam String fileName, @RequestParam MultipartFile file) {
-        return fileService.upload(bucketName, fileName, file);
+    public String upload(@RequestParam String bucketName, @RequestParam String filePath, @RequestParam MultipartFile file) {
+        return fileService.upload(bucketName, filePath, file);
     }
+
+    @RequestMapping(value = "/uploads", method = RequestMethod.POST)
+    public String uploads(@RequestParam String bucketName, @RequestParam String filePath, @RequestParam MultipartFile[] files) {
+        return fileService.uploads(bucketName, filePath, files);
+    }
+
+    @RequestMapping(value = "/delete", method = RequestMethod.POST)
+    public String delete(@RequestParam String bucketName, @RequestParam String objectName) {
+        return fileService.delete(bucketName, objectName);
+
+    }
+
+    @RequestMapping(value = "/deletefiles", method = RequestMethod.POST)
+    public String deletefiles(@RequestParam String bucketName, @RequestParam String[] objectName) {
+        return fileService.deleteFiles(bucketName, objectName);
+
+    }
+
+    @RequestMapping(value = "/download", method = RequestMethod.POST)
+    public String download(@RequestParam String bucketName, @RequestParam String objectName, @RequestParam String localPath) {
+        return fileService.download(bucketName, objectName, localPath);
+    }
+
 
 }
--- src/main/java/com/huayun/ossagent/service/FileService.java
+++ src/main/java/com/huayun/ossagent/service/FileService.java
@@ -11,6 +11,7 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
@@ -29,6 +30,8 @@ public class FileService {
             client.createBucket(createBucketRequest);
             createBucketTips = "ok," + bucketName + " 存储桶创建成功！";
         }
+        client.shutdown();
+
         return createBucketTips;
     }
 
@@ -43,6 +46,8 @@ public class FileService {
         OSSClient client = ossClient.getOssClient();
 
         List<Bucket> buckets = client.listBuckets();
+        client.shutdown();
+
         return buckets;
     }
 
@@ -57,7 +62,9 @@ public class FileService {
         // 获取指定bucket下的所有Object信息
         ObjectListing objectListing = client.listObjects(bucketName);
         List<OSSObjectSummary> lists = objectListing.getObjectSummaries();
+        client.shutdown();
         return lists;
+
     }
 
     /**
@@ -76,6 +83,7 @@ public class FileService {
             client.deleteBucket(bucketName);
             deleteBucketTips = "ok," + bucketName + "删除成功！";
         }
+        client.shutdown();
         return deleteBucketTips;
     }
 
@@ -92,7 +100,7 @@ public class FileService {
         String uploadTips = "";
         OSSClient client = ossClient.getOssClient();
         String fileName = file.getOriginalFilename();
-        String uploadPath =  basePath + "/" + fileName;
+        String uploadPath = basePath + "/" + fileName;
         boolean exists = client.doesBucketExist(bucketName);
         if (exists) {
             // 创建PutObjectRequest对象。
@@ -109,11 +117,162 @@ public class FileService {
         } else {
             uploadTips = "文件 " + bucketName + " 上传失败！";
         }
+        client.shutdown();
         return uploadTips;
     }
 
-//    public static void main(String[] args) {
-//        FileService fs = new FileService();
-//        fs.getFiles("file-service");
+    public String uploads(String bucketName, String filePath, MultipartFile[] files) {
+        String uploadTips = "";
+        OSSClient client = ossClient.getOssClient();
+        boolean exists = client.doesBucketExist(bucketName);
+        if (exists) {
+            for (int i = 0; i < files.length; i++) {
+                String fileName = files[i].getOriginalFilename();
+                // 先判断是否直接存储在桶下
+                InputStream inputStream = null;
+                try {
+                    inputStream = files[i].getInputStream();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                // 如果是直接上传到存储桶下，传入filePath = "/"即可
+                if (filePath.equals("/")) {
+                    PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, fileName, inputStream);
+                    client.putObject(putObjectRequest);
+                    uploadTips += fileName + ",";
+                } else { // 如果是上传到存储桶指定的子集目录下，形如filePath = "xxx/yyy"格式即可
+                    String uploadPath = filePath + "/" + fileName;
+                    PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, uploadPath, inputStream);
+                    client.putObject(putObjectRequest);
+                }
+            }
+            uploadTips = "OK! " + uploadTips + " 上传成功！";
+        } else {
+            uploadTips = "文件 " + bucketName + " 上传失败！";
+        }
+        client.shutdown();
+        return uploadTips;
+    }
+
+    /**
+     * 删除单个文件
+     *
+     * @param bucketName 存储桶名
+     * @param objectName 指定存储桶下的文件对象
+     * @return
+     */
+    public String delete(String bucketName, String objectName) {
+        String deleteTips = "";
+        OSSClient client = ossClient.getOssClient();
+        boolean bucketExist = client.doesBucketExist(bucketName);
+        // 判断存储桶和文件对象是否存在
+        boolean objectExist = client.doesObjectExist(bucketName, objectName);
+        if (bucketExist && objectExist) {
+            client.deleteObject(bucketName, objectName);
+            deleteTips = objectName + "，文件删除成功！";
+        } else {
+            deleteTips = objectName + "，文件删除失败！，该对象不存在";
+        }
+        client.shutdown();
+        return deleteTips;
+    }
+
+    /**
+     * bucketName  存储桶名称
+     * objects     删除文件的字符
+     *
+     * @return
+     */
+    // 每一个文件对象以";"分割
+//    public String deleteFiles(String bucketName, String objects) {
+//        String deleteTips = "";
+//        OSSClient client = ossClient.getOssClient();
+//        List<String> keys = new ArrayList<String>();
+//        String[] arr = objects.split(";");
+//        for (int i = 0; i < arr.length; i++) {
+//            keys.add(arr[i]);
+//        }
+//        DeleteObjectsResult deleteObjectsResult = client.deleteObjects(new DeleteObjectsRequest(bucketName).withKeys(keys));
+//        List<String> deletedObjects = deleteObjectsResult.getDeletedObjects();
+//        deleteTips = "文件批量删除成功!";
+//        client.shutdown();
+//        return deleteTips;
 //    }
+    public String deleteFiles(String bucketName, String[] files) {
+        String deleteTips = "";
+        OSSClient client = ossClient.getOssClient();
+        List<String> keys = new ArrayList<>();
+        for (int i = 0; i < files.length; i++) {
+            keys.add(files[i]);
+        }
+        DeleteObjectsResult deleteObjectsResult = client.deleteObjects(new DeleteObjectsRequest(bucketName).withKeys(keys));
+        List<String> deletedObjects = deleteObjectsResult.getDeletedObjects();
+        deleteTips = "文件批量删除成功!";
+        client.shutdown();
+        return deleteTips;
+    }
+
+    /**
+     * 文件下载功能
+     *
+     * @param bucketName 存储桶名
+     * @param objectName oss服务的文件对象名
+     * @param localPath  oss服务的文件对象名
+     * @return
+     */
+    public String download(String bucketName, String objectName, String localPath) {
+        String downloadTips = "";
+        OSSClient client = ossClient.getOssClient();
+        boolean exist = client.doesBucketExist(bucketName);
+        boolean bucketExist = client.doesObjectExist(bucketName, objectName);
+        if (exist && bucketExist) {
+            // 指定文件保存路径
+            File file = null;
+            String filePath = "";
+//            if (objectName.contains("/")) {
+//                //判断文件目录是否存在，不存在则创建
+//                filePath = localPath;
+//            } else { // objectName没有"/"这种情况，也即objectName对象直接存储在存储桶一级目录下
+//                filePath = localPath + "/" + objectName;
+//            }
+            filePath = localPath;
+            file = new File(filePath);
+            if (!file.exists()) {
+                file.mkdirs();
+            }
+            //判断保存文件名是否加后缀
+            if (objectName.contains(".")) {
+                //指定文件保存名称
+                filePath = filePath + "/" + objectName.substring(objectName.lastIndexOf("/") + 1);
+            }
+            //获取OSS文件并保存到本地指定路径中，此文件路径一定要存在，若保存目录不存在则报错，若保存文件名已存在则覆盖本地文件
+            client.getObject(new GetObjectRequest(bucketName, objectName), new File(filePath));
+            downloadTips = "oK，" + objectName + "文件下载成功";
+        } else {
+
+            downloadTips = "下载文件失败!";
+        }
+        client.shutdown();
+        return downloadTips;
+    }
+
+
+    public static void main(String[] args) {
+        FileService fileService = new FileService();
+        String fileName = "abc/efg/fff.doc";
+        String filePath1 = fileName + "/" + fileName.substring(0, fileName.lastIndexOf("/"));
+
+        String filePath2 = fileName.substring(fileName.lastIndexOf("/") + 1);
+        System.out.println(filePath1);
+        System.out.println(filePath2);
+//        fileService.download("oss-xl", "1.png");
+
+//        String a = "abc, 123,fff";
+//        String[] arr = a.split(";");
+//        List<String> keys = new ArrayList<String>();
+//        for (int i = 0; i < arr.length; i++) {
+//            keys.add(arr[i]);
+//        }
+//        System.out.println(keys);
+    }
 }
